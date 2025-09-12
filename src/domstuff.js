c1c37c8d5b4573a74fa651204a47bd583adbd4cb
Alex <alex.holder@bigfootds.com>
2025-09-13 09:38:52 +1000

    feat: create a breakfastmenucontainer parent element

--- src/domstuff.js
+++ src/domstuff.js
@@ -15,3 +15,44 @@ function paragraphTagUpdater(){
 }
 
 paragraphTagUpdater();
+
+
+
+
+// Breakfast Menu renderer 
+
+let breakfastMenuItems = [
+	"pancakes",
+	"blueberry pancakes",
+	"bacon and eggs",
+	"omelette",
+	"avo smash"
+];
+
+function breakfastRenderer () {
+	// 1. render the breakfast menu container
+	// 1a. find the menu container's parent element
+	let targetParentContainer = document.querySelector("body");
+	// 1b. create the menu container element 
+	let menuContainer = document.createElement("section");
+	console.log(menuContainer);
+	menuContainer.id = "breakfastmenu";
+	// 1c. append the menu container element to the parent element 
+	targetParentContainer.appendChild(menuContainer);
+	// document.body.appendChild()
+
+
+	// 2. create menu options 
+
+	// 3. insert menu options into breakfast menu container 
+}
+
+breakfastRenderer();
+
+
+
+
+
+
+
+
